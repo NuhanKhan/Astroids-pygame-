e30b43de7765e7b9286dd9b9bbb4b3e0925ec59d
NuhanKhan <nuhankhan360@gmail.com>
2026-06-19 05:01:21 +0600

    D: we have updated the player function in a way so that we can add both left and right rotation and up and down movement and mix them together so that you can basically steer your triangle like you steer a car.

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def main():
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000  # Convert milliseconds to seconds
+        player.update(dt)
         #print(dt)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-from constants import LINE_WIDTH, PLAYER_RADIUS
+from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame
 from circleshape import CircleShape
 
@@ -6,12 +6,22 @@ class Player(CircleShape):
     def __init__(self, x: float, y: float) -> None:
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0.0  # in degrees
-        self.width = LINE_WIDTH
 
     def draw(self, screen: pygame.Surface) -> None:
-        pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), self.width)
-    
+        pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), LINE_WIDTH)
+
+    def rotate(self, direction: int, dt: float) -> None:
+        self.rotation += direction * PLAYER_TURN_SPEED * dt
+
+    def move(self, dt: float) -> None:
         # in the Player class
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
+
+
+
     def triangle(self) -> list[pygame.Vector2]:
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -19,3 +29,15 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
+    def update(self, dt: float) -> None:
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-1, dt)
+        if keys[pygame.K_d]:
+            self.rotate(1, dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
